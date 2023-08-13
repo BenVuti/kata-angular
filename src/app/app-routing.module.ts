4cbd3be1eeadf34943768e304a8fce1fd5f4ba48
Théo BENVENUTI <tbenvenuti_ext@altareacogedim.com>
2023-08-13 16:39:57 +0200

    Add the step components and the form-data service + lint project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Step1Component } from './step1/step1.component';
+import { Step2Component } from './step2/step2.component';
+import { Step3Component } from './step3/step3.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'step1', pathMatch: 'full' },
+  { path: 'step1', component: Step1Component },
+  { path: 'step2', component: Step2Component },
+  { path: 'step3', component: Step3Component },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
