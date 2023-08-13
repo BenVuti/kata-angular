1aaea9e939247bcdb35509c6fe33e489e1996e25
Théo BENVENUTI <tbenvenuti_ext@altareacogedim.com>
2023-08-13 18:32:57 +0200

    Add step 2

--- src/app/step2/step2.component.spec.ts
+++ src/app/step2/step2.component.spec.ts
@@ -1,16 +1,35 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import {
+  MatSelectModule,
+  MatOptionModule,
+  MatFormFieldModule,
+  MatInputModule,
+} from '@angular/material';
 import { Step2Component } from './step2.component';
 
 describe('Step2Component', () => {
   let component: Step2Component;
   let fixture: ComponentFixture<Step2Component>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
       declarations: [Step2Component],
+      imports: [
+        NoopAnimationsModule,
+        ReactiveFormsModule,
+        MatSelectModule,
+        MatOptionModule,
+        MatInputModule,
+        MatFormFieldModule,
+      ],
+      providers: [FormBuilder],
     }).compileComponents();
+  }));
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(Step2Component);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -18,5 +37,77 @@ describe('Step2Component', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have a defined form', () => {
+    expect(component.form).toBeDefined();
+  });
+
+  it('should create the form with required fields', () => {
+    expect(component.form).toBeTruthy();
+    expect(component.form.controls['ownershipStatus']).toBeTruthy();
+    expect(component.form.controls['householdSize']).toBeTruthy();
+    expect(component.form.controls['householdIncome']).toBeTruthy();
+    expect(component.form.controls['propertySize']).toBeTruthy();
+  });
+
+  it('should validate the form fields are required', () => {
+    const form = component.form;
+    form.setValue({
+      ownershipStatus: '',
+      householdSize: null,
+      householdIncome: null,
+      propertySize: null,
+    });
+    expect(form.valid).toBeFalsy();
+    expect(form.controls['ownershipStatus'].valid).toBeFalsy();
+    expect(form.controls['householdSize'].valid).toBeFalsy();
+    expect(form.controls['householdIncome'].valid).toBeFalsy();
+    expect(form.controls['propertySize'].valid).toBeFalsy();
+  });
+
+  it('should submit the form when valid', () => {
+    spyOn(console, 'log');
+    const submitButton = fixture.nativeElement.querySelector(
+      'button[type="submit"]'
+    );
+
+    component.form.patchValue({
+      ownershipStatus: 'owner',
+      householdSize: 3,
+      householdIncome: 50000,
+      propertySize: 150,
+    });
+
+    fixture.detectChanges();
+
+    submitButton.click();
+    fixture.detectChanges();
+
+    expect(console.log).toHaveBeenCalledWith(
+      'Form step 2 submitted:',
+      component.form.value
+    );
+  });
+
+  it('should not submit the form when invalid', () => {
+    spyOn(console, 'log');
+    const submitButton = fixture.nativeElement.querySelector(
+      'button[type="submit"]'
+    );
+
+    component.form.patchValue({
+      ownershipStatus: '',
+      householdSize: 0,
+      householdIncome: 200000,
+      propertySize: -10,
+    });
+
+    fixture.detectChanges();
+
+    submitButton.click();
+    fixture.detectChanges();
+
+    expect(console.log).not.toHaveBeenCalled();
   });
 });
--- src/app/step2/step2.component.ts
+++ src/app/step2/step2.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Person } from 'src/models/person.model';
+import { FormDataService } from 'src/services/form-data.service';
 
 @Component({
   selector: 'app-step2',
@@ -6,7 +10,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./step2.component.scss'],
 })
 export class Step2Component implements OnInit {
-  constructor() {}
+  form!: FormGroup;
+  person!: Person;
 
-  ngOnInit(): void {}
+  constructor(
+    private formDataService: FormDataService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    if (!this.formDataService.formData.get('step1')?.valid) {
+      this.router.navigate(['/step1']);
+      return;
+    }
+
+    this.person = this.formDataService.getPesron();
+
+    this.form = this.formDataService.formData.get('step2') as FormGroup;
+  }
+
+  onNextClick(): void {
+    if (this.form.valid) {
+      console.log('Form step 2 submitted:', this.form.value);
+
+      this.router.navigate(['/step3']);
+    } else {
+      // Display validation errors or prevent form submission
+    }
+  }
 }
